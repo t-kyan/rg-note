1e7afec111e8b94a298706624d4c8b3dc8bd9cd3
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-09-15 23:22:29 +0900

    userモデルへバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :customers
+
+  with_options presence: true do
+    validates :store_name
+    validates :manager
+  end
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  }
 end
