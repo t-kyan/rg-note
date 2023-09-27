71942683d9a74d9bf4f203a4e262fbc8929bb28e
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-09-27 18:15:59 +0900

    customersコントローラー　アクション追加
    ログイン認証の記述　authenticate_user!

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,11 @@
 class CustomersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:index, :new]
 
   def index
+    @customers = Customer.all
+  end
+
+  def new
+    @Customer = Customer.new
   end
 end
