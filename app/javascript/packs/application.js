52ecaa17105a27779a9b7ee30a1598a519583ad4
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-09-28 02:43:20 +0900

    カレンダー表示（bootstrap-datepickerの導入）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
 import "../stylesheets/application"
+import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js';
+import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.ja.min.js';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
