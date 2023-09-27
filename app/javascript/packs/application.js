c0572a8704a40e79fd941d8488d360a11b4bb99f
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-09-28 05:34:03 +0900

    jQueryの導入（Bootstrap DatePickerを使用するため）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
-import "../stylesheets/application"
-import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js';
-import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.ja.min.js';
-
+import "jquery";
+import "bootstrap";
+import "../stylesheets/application";
+import "bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js";
+import "bootstrap-datepicker/dist/locales/bootstrap-datepicker.ja.min.js"; // ロケールファイルをインポート
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
