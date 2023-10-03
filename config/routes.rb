cfff9cca8aa8ef11724cbfd451fd873d10758db5
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-10-03 16:55:11 +0900

    ログアウトができなくなった為ルーティングに追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_scope :user do
     root to: 'devise/sessions#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
   devise_for :users
   resources :customers, only: [:index, :new, :create]
