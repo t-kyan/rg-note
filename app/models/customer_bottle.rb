3f491271071c41e703d48f3d11e95ff84bc6cb2c
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-10-04 05:07:50 +0900

    Formオブジェクトパターンで実装

---
+++ app/models/customer_bottle.rb
@@ -0,0 +1,18 @@
+class CustomerBotlle
+  include ActiveModel::Model
+  attr_accessor :name, :name_kana, :coming_date, :birth_date, :company, :phone, :address, :friend, :server, :memo, :bottle_name, :control_number, :amount_in_bottle, :opening_date, :user_id
+
+  with_options presence: true do
+    validates :name
+    validates :name_kana, format: {with: /\A[ァ-ヶー]+\z/}
+    validates :user_id
+  end
+  validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+
+  def save
+    customer = Customer.create(user_id: user_id, name: name, name_kana: name_kana, coming_date: coming_date, birth_date: birth_date, company: company, phone: phone, address: address, friend: friend,
+    server: server, memo: memo)
+    Bottle.create(bottle_name: bottle_name, control_number: control_number, amount_in_bottle: amount_in_bottle, opening_date: opening_date, customer_id: customer.id)
+  end
+
+end
