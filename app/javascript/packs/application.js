0d2b43c5935e983b0188631da304f9f08b7dc866
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-10-04 05:08:21 +0900

    読み込みができないため記述を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ import "jquery";
 import "bootstrap";
 import "../stylesheets/application";
 import "bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js";
-import "bootstrap-datepicker/dist/locales/bootstrap-datepicker.ja.min.js"; // ロケールファイルをインポート
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
