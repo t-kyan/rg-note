e637233272faf1df6e7ccfc25541b92eda5b83d3
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-10-07 05:44:38 +0900

    JSファイル作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ import "jquery";
 import "bootstrap";
 import "../stylesheets/application";
 import "bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js";
+require("../customer");
+require("../bottle");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
