9aa0243ce9b1071f4524870e8c0801853530a726
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-10-11 02:30:42 +0900

    顧客一覧の表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def days_since_opening(opening_date)
+    return unless opening_date
+    # 現在の日付を取得
+    current_date = Date.today
+    # 開封日と現在の日付の差を計算
+    days_difference = (current_date - opening_date).to_i
+    # 経過日数を返す
+    days_difference
+  end
 end
